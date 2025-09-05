ec488e42d5f0b6318b9884d3a2d84f5505588398
LeeMengChieh <aphmiku@gmail.com>
2025-09-05 10:29:03 +0800

    修正首頁動畫JS

---
+++ js/IndexJS.js
@@ -0,0 +1,47 @@
+  document.addEventListener('DOMContentLoaded', () => {
+      if (window.innerWidth >= 768) {
+        startMarquee();
+      }
+
+      function startMarquee() {
+        const marquee = document.querySelector('.marquee');
+        let left = 0;
+        let timer;
+
+        const firstImg = marquee.querySelector('img');
+        firstImg.onload = () => startScroll();
+        if (firstImg.complete) startScroll();
+
+        function startScroll() {
+          function getImgWidth() {
+            return marquee.querySelector('img').clientWidth;
+          }
+
+          function move() {
+            clearInterval(timer);
+            const imgWidth = getImgWidth();
+            const duration = 20000; // 20 秒
+            const interval = 20;
+            const step = imgWidth / (duration / interval);
+
+            timer = setInterval(() => {
+              left -= step;
+              if (left <= -imgWidth) left = 0;
+              marquee.style.left = `${left}px`;
+            }, interval);
+          }
+
+          move();
+
+          window.addEventListener('resize', () => {
+            if (window.innerWidth >= 768) {
+              left = 0;
+              marquee.style.left = '0px';
+              move();
+            } else {
+              clearInterval(timer);
+            }
+          });
+        }
+      }
+    });
